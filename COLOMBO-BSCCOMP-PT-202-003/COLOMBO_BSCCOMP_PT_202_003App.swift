b4a0dd766fecad15bfeda8ae4621fd8333710033
SahanRockMaster <cobsccomp202p003@student.nibm.lk>
2022-04-06 01:31:00 +0530

    Initial Commit

---
+++ COLOMBO-BSCCOMP-PT-202-003/COLOMBO_BSCCOMP_PT_202_003App.swift
@@ -0,0 +1,17 @@
+//
+//  COLOMBO_BSCCOMP_PT_202_003App.swift
+//  COLOMBO-BSCCOMP-PT-202-003
+//
+//  Created by Sahan Ruwanga on 2022-04-06.
+//
+
+import SwiftUI
+
+@main
+struct COLOMBO_BSCCOMP_PT_202_003App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
