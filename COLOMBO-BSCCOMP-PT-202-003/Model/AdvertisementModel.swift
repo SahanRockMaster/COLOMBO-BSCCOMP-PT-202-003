42adaa0b3763ba47c8d4cbb25207deb458487aaf
SahanRockMaster <cobsccomp202p003@student.nibm.lk>
2022-04-17 03:45:33 +0530

    Next Commit V1.2

---
+++ COLOMBO-BSCCOMP-PT-202-003/Model/AdvertisementModel.swift
@@ -0,0 +1,40 @@
+//
+//  AdvertisementModel.swift
+//  COLOMBO-BSCCOMP-PT-202-003
+//
+//  Created by Sahan Ruwanga on 2022-04-15.
+//
+
+import Foundation
+import SwiftUI
+struct AdvertisementModel{
+    var price: Double
+    var landORhouse: String
+    var landSize: Double
+    var district: String
+    var townVillage: String
+}
+
+struct PickerAdvertisements{
+    var selected : [UIImage]
+    var show : Bool
+}
+
+struct SingleImage{
+    var images : Data
+    var imagePicker : Bool
+}
+
+struct PostAds: Identifiable{
+    var id = UUID().uuidString
+    var price: Double
+    var landORhouse: String
+    var landSize: Double
+    var district: String
+    var townVillage: String
+    var dateCreated : Date
+    var imagesArray : [String]
+    var province : String
+    var deedImage : String
+    var geo_location: String
+}
---
+++ COLOMBO-BSCCOMP-PT-202-003/Model/ErrorMessageModel.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorMessageModel.swift
+//  COLOMBO-BSCCOMP-PT-202-003
+//
+//  Created by Sahan Ruwanga on 2022-04-15.
+//
+
+import Foundation
+import SwiftUI
+
+struct ErrorMessageModel {
+    var alert : Bool
+    var error : String
+    var topic : String
+    var isLoading : Bool
+    var guestUser: Bool
+}
