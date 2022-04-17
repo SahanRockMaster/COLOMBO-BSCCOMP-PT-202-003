cf283a273f87eda5ef85726ceb0163326363d3b1
SahanRockMaster <cobsccomp202p003@student.nibm.lk>
2022-04-17 07:59:35 +0530

    Next Commit V1.3

---
+++ COLOMBO-BSCCOMP-PT-202-003/Helpers/Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  Extensions.swift
+//  COLOMBO-BSCCOMP-PT-202-003
+//
+//  Created by Sahan Ruwanga on 2022-04-15.
+//
+
+import Foundation
+//Regex Validation As String Extentsions
+
+extension String {
+    var isValidContact: Bool {
+        let phoneNumberRegex = "^\\d{10}$"
+        let phoneTest = NSPredicate(format: "SELF MATCHES %@", phoneNumberRegex)
+        let isValidPhone = phoneTest.evaluate(with: self)
+        return isValidPhone
+    }
+    var isValidNIC: Bool {
+        let nicRegex = "^\\d{9}$"
+        let nicTest = NSPredicate(format: "SELF MATCHES %@", nicRegex)
+        let isValidNIC = nicTest.evaluate(with: self)
+        return isValidNIC
+    }
+}
--- COLOMBO-BSCCOMP-PT-202-003/View/SettingView.swift
+++ COLOMBO-BSCCOMP-PT-202-003/View/SettingView.swift
@@ -10,39 +10,84 @@ import Firebase
 
 struct SettingView: View {
     @StateObject private var settingVM = SettingViewModel()
+    @State var visible = false
     
     var body: some View {
         ZStack{
-            VStack{
-            
-                Form{
-                Section(header: Text("Personal Infromation")) {
-                    TextField("Name", text: $settingVM.userDetails.name).textInputAutocapitalization(.never)
-                    TextField("NIC", text: $settingVM.userDetails.nic).textInputAutocapitalization(.never)
-                    TextField("Mobile Number", text: $settingVM.userDetails.mobile).textInputAutocapitalization(.never)
-                    Text(settingVM.userDetails.email).foregroundColor(.gray)
-                    Text(settingVM.userDetails.selectedGender).foregroundColor(.gray)
-                }
+            ZStack(alignment: .topLeading, content: {
+                GeometryReader{_ in
+                    ScrollView{
+                        LazyVStack{
+//                Section(header:
+                            Text("Personal Infromation")
+//                    TextField("Name", text: $settingVM.userDetails.name).textInputAutocapitalization(.never)
+                    TextField ("Name", text: self.$settingVM.userDetails.name)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 4)
+                                .stroke(self.settingVM.userDetails.name != "" ? Color.cyan: Color.cyan.opacity(0.7), lineWidth: 2)
+                        )
+                        .padding(.top, 5)
+//                    TextField("NIC", text: $settingVM.userDetails.nic).textInputAutocapitalization(.never)
+                    TextField ("NIC", text: self.$settingVM.userDetails.name)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 4)
+                                .stroke(self.settingVM.userDetails.nic != "" ? Color.cyan: Color.cyan.opacity(0.7), lineWidth: 2)
+                        )
+                        .padding(.top, 5)
+//                    TextField("Mobile Number", text: $settingVM.userDetails.mobile).textInputAutocapitalization(.never)
+                    TextField ("Mobile Number", text: self.$settingVM.userDetails.mobile)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 4)
+                                .stroke(self.settingVM.userDetails.nic != "" ? Color.cyan: Color.cyan.opacity(0.7), lineWidth: 2)
+                        )
+                        .padding(.top, 5)
+//                    Text(settingVM.userDetails.email).foregroundColor(.gray)
+                    Text(settingVM.userDetails.email)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 4)
+                                .stroke(self.settingVM.userDetails.email != "" ? Color.cyan: Color.cyan.opacity(0.7), lineWidth: 2)
+                        )
+                        .padding(.top, 5)
+                            Text(settingVM.userDetails.selectedGender)
+                                .padding()
+                                .background(
+                                    RoundedRectangle(cornerRadius: 4)
+                                        .stroke(self.settingVM.userDetails.selectedGender != "" ? Color.cyan: Color.cyan.opacity(0.7), lineWidth: 2)
+                                )
+                                .padding()
+                                .padding(.top, 5)
+//                        .padding()
+//                        .background(
+//                            RoundedRectangle(cornerRadius: 4)
+//                                .fill(self.settingVM.userDetails.selectedGender != "" ? Color.cyan: Color.cyan.opacity(0.7))
+//                        )
+//                        .padding(.top, 5)
+//                    Text(settingVM.userDetails.selectedGender).foregroundColor(.gray)
+                
 
-                Section(header: Text("Action")){
+//             Text("Action"){
                     Button {
                         self.settingVM.logout()
                     } label: {
                         Text("Log out")
                             .foregroundColor(.red)
                     }
-                }
-            }
-        }
+             
+                        
+//            }
+//        }
             if self.settingVM.message.alert{
                GeometryReader{_ in
                     ErrorMessagesView(alert: self.$settingVM.message.alert, error: self.$settingVM.message.error, topic: self.$settingVM.message.topic, loading: self.$settingVM.message.isLoading, guestUser: $settingVM.message.guestUser)
                }
             }
         }
-        .toolbar{
-            ToolbarItemGroup(placement: .navigationBarTrailing) {
-                Button {
+                
+                   Button {
                     self.settingVM.verify()
                 } label: {
                     Text("Save")
@@ -50,13 +95,16 @@ struct SettingView: View {
 
             }
         }
+    
+        })
+            }
+        
     }
 }
+//struct SettingView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SettingView()
+//      }
+//    }
 
-struct SettingView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingView()
-    }
-}
-
-
+            
